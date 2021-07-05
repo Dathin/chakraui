3c0aac3c1ba8c9110edf2b2a5a083b1f32407989
Pedro Caires <pedro.caires@iteris.com.br>
2021-07-05 19:51:31 -0300

    fix: responsividade

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -4,24 +4,24 @@ import { Airplane } from '../Airplane'
 //Fazer o avião sair do coiso
 export function Banner(){
 
-    const isMediumScreen = useBreakpointValue({
-        md: true,
-    });
+    // const isMediumScreen = useBreakpointValue({
+    //     md: true,
+    // });
 
-    const isLargeScreen = useBreakpointValue({
-        lg: true,
-    });
+    // const isLargeScreen = useBreakpointValue({
+    //     lg: true,
+    // });
 
     return (
         <Box backgroundImage="./bannerbackfround.png" w="100%"  backgroundRepeat="no-repeat" backgroundSize="cover" color="white.100" px={4} pt={25} pb={19}>
-            <Flex justify={isLargeScreen ? "space-between" : "center"} w="100%" flexWrap={isLargeScreen ? "nowrap" : "wrap"}>    
+            <Flex justify={["center", "center", "space-between"]} w="100%" flexWrap={["wrap", "wrap", "nowrap"]}>    
             <Center>
                 <Box>
-                    <Heading fontWeight="medium" lineHeight={isMediumScreen ? '54px' : '30px'} as="h1" fontSize={isMediumScreen ? 36 : 20}>5 continentes,  infinitas possibilidades</Heading>
+                    <Heading fontWeight="medium" lineHeight={["30px","54px"]} as="h1" fontSize={[20, 36]}>5 continentes,  infinitas possibilidades</Heading>
                     <Text fontSize={14} lineHeight="21px">Chegou a hora de tirar do papel a viagem que você sempre sonhou.</Text>
                 </Box>
             </Center>
-            {isMediumScreen && <Box transform="rotate(6deg)">
+            {false && <Box transform="rotate(6deg)">
                 <Icon as={Airplane} />
             </Box>}
 
