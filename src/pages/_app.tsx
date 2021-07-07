12a67381dd246b218e574a64f94cfac7330598c8
Pedro Caires <pedro.caires@iteris.com.br>
2021-07-07 14:52:05 -0300

    fix: import place

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,27 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import type { AppProps } from 'next/app'
-import React from 'react'
-import { Header } from '../components/Header'
-import { HeaderProvider } from '../contexts/HeaderContext'
-import theme from '../styles/theme'
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import React from "react";
+import { Header } from "../components/Header";
+import { HeaderProvider } from "../contexts/HeaderContext";
+import theme from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-  <ChakraProvider theme={theme}>
-    <HeaderProvider>
-      <Header />
-      <Component {...pageProps} />
-    </HeaderProvider>
-  </ChakraProvider>  )
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <HeaderProvider>
+          <Header />
+          <Component {...pageProps} />
+        </HeaderProvider>
+      </ChakraProvider>
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
