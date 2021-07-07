b5301abb582212d5f440030b8f2c87ae3062bf28
Pedro Caires <pedro.caires@iteris.com.br>
2021-07-07 14:45:16 -0300

    fix: add meta viewport

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,7 @@ export default class MyDocument extends Document {
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@500;600&family=Poppins:wght@400;500&display=swap" rel="stylesheet" />
                 <link rel="shortcut icon" href="./favicon.svg" type="image/svg+xml" />
+                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
             </Head>
             <body>
                 <Main />
