e45d64a9dc62dd1fa4eb975e8b3e0a5cae047181
Pedro Caires <pedro.caires@iteris.com.br>
2021-07-08 00:17:15 -0300

    feat: cities simple grid

--- src/components/Cities/index.tsx
+++ src/components/Cities/index.tsx
@@ -1,3 +1,7 @@
+import { Avatar, Box, Center, Flex, SimpleGrid, Stack } from "@chakra-ui/react";
+import React from "react";
+import Image from 'next/image'
+
 interface CitiesProps {
     top100Cities: {
           image: string,
@@ -8,5 +12,21 @@ interface CitiesProps {
 }
 
 export function Cities({top100Cities}: CitiesProps){
-    return <h1>xD</h1>
+    return <Flex w="100%" justifyContent="center" pt={9} mb={9}>
+        <SimpleGrid columns={1} spacing={10} >
+            {/* {top100Cities.map(() => )} */}
+            <Stack width="256" spacing={0}>
+                <Image width="256" height="173" src="/rome.png" alt="Roma" />
+                <Flex w="100%" p={4} border="1px solid #FFBA08" borderBottomRadius={3} borderTop="none">
+                    <Stack w="50%">
+                        <div>Paris</div>
+                        <div>França</div>
+                    </Stack>
+                    <Flex w="50%" justifyContent="center" alignItems="center">
+                        <Avatar name="defaultcontryavatar" width="30" height="30" src="/defaulcountryavatar.png" />
+                    </Flex>
+                </Flex>
+            </Stack>
+        </SimpleGrid>
+    </Flex>
 }
