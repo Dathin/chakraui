31b232b3ea4b593d0733e24b30015eb995677a99
Pedro Caires <pedro.caires@iteris.com.br>
2021-07-08 00:23:37 -0300

    fix: tornando dinamico as cidades

--- src/components/Cities/index.tsx
+++ src/components/Cities/index.tsx
@@ -14,19 +14,18 @@ interface CitiesProps {
 export function Cities({top100Cities}: CitiesProps){
     return <Flex w="100%" justifyContent="center" pt={9} mb={9}>
         <SimpleGrid columns={1} spacing={10} >
-            {/* {top100Cities.map(() => )} */}
-            <Stack width="256" spacing={0}>
-                <Image width="256" height="173" src="/rome.png" alt="Roma" />
+            {top100Cities.map(({image, name, countryName, countryImage}, index) => <Stack width="256" spacing={0} key={index}>
+                <Image width="256" height="173" src={image} alt={name} />
                 <Flex w="100%" p={4} border="1px solid #FFBA08" borderBottomRadius={3} borderTop="none">
                     <Stack w="50%">
-                        <div>Paris</div>
-                        <div>França</div>
+                        <Box>{name}</Box>
+                        <Box>{countryName}</Box>
                     </Stack>
                     <Flex w="50%" justifyContent="center" alignItems="center">
-                        <Avatar name="defaultcontryavatar" width="30" height="30" src="/defaulcountryavatar.png" />
+                        <Avatar name="defaultcontryavatar" width="30" height="30" src={countryImage} />
                     </Flex>
                 </Flex>
-            </Stack>
+            </Stack>)}
         </SimpleGrid>
     </Flex>
 }
